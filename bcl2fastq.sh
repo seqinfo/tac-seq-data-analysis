91eb5a3e607f3234453a19d76d824c5a9710d769
Hindrek Teder <hindrek.teder@gmail.com>
2018-02-07 19:23:33 +0200

    remove sbatch info

--- bcl2fastq.sh
+++ bcl2fastq.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-#SBATCH --job-name=bcl2fastq
-#SBATCH --partition=main
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32000
-#SBATCH --time=02:00:00
-
-module load bcl2fastq2-2.20
-
 # arguments with default values
 RUNFOLDER=
 OUTPUT=
