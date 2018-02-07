b58ee6fbf77847938e75b11971d97fc61a30f815
Hindrek Teder <hindrek.teder@gmail.com>
2018-02-07 19:35:16 +0200

    Delete bcl2fastq.sh

--- bcl2fastq.sh
+++
@@ -1,7 +0,0 @@
-# arguments with default values
-RUNFOLDER=
-OUTPUT=
-MISMATCHES=0
-
-# convert BLCs to FASTQs, merge lanes
-bcl2fastq -R $RUNFOLDER -o $OUTPUT --barcode-mismatches $MISMATCHES --no-lane-splitting
