0b99d34132e06ddc13527e8327db11455163e0b4
Hindrek Teder <hindrek.teder@gmail.com>
2018-02-12 22:46:38 +0200

    fix input

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ OUTPUT=$3
 MISMATCHES=$4  # number of allowed mismatches per target sequence
 
 # read arguments for count.sh
-INPUT2=$OUTPUT  # path to sample.sh output folder
+INPUT2=$OUTPUT  # sample.sh output folder
 UMI=$5  # UMI threshold
 
-source sample.sh $INPUT1 $TARGET $OUTPUT $MISMATCHES
+source sample.sh "$INPUT1" $TARGET $OUTPUT $MISMATCHES
 source count.sh $INPUT2 $UMI > $OUTPUT/counts_UMI$UMI.tsv
 echo "results are in $OUTPUT/counts_UMI$UMI.tsv"
--- sample.sh
+++ sample.sh
@@ -7,48 +7,40 @@ OUTPUT=$3
 MISMATCHES=$4  # number of allowed mismatches per target sequence
 
 mkdir $OUTPUT
-cd $OUTPUT
-
 for i in $INPUT; do
 	# compressed FASTQ
 	if [[ $i == *.gz ]]; then
-		sample=$(basename $i .fastq.gz)  # remove .fastq.gz from sample name
-		mkdir $sample
-		cd $sample
-		
 		# convert FASTQ to FASTA, trim to 62 bp, keep 62 bp reads
-		zcat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > trimmed.fasta
+		sample=$(basename $i .fastq.gz)  # remove .fastq.gz from sample name
+		mkdir $OUTPUT/$sample
+		zcat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > $OUTPUT/$sample/trimmed.fasta
 	
 	# uncompressed FASTQ
 	else
-		sample=$(basename $i .fastq)  # remove .fastq from sample name
-		mkdir $sample
-		cd $sample
-		
 		# convert FASTQ to FASTA, trim to 62 bp, keep 62 bp reads
-		cat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > trimmed.fasta
+		sample=$(basename $i .fastq)  # remove .fastq from sample name
+		mkdir $OUTPUT/$sample
+		cat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > $OUTPUT/$sample/trimmed.fasta
 	fi
 	
 	# join UMI subsequences
-	awk '{if (/^>/) print $0; else printf "%s%s%s\n", substr($0,1,4), substr($0,59,4), substr($0,5,54)}' trimmed.fasta > umi_joined.fasta
+	awk '{if (/^>/) print $0; else printf "%s%s%s\n", substr($0,1,4), substr($0,59,4), substr($0,5,54)}' $OUTPUT/$sample/trimmed.fasta > $OUTPUT/$sample/umi_joined.fasta
     
 	# demultiplex by locus
-	mkdir loci
-	cat umi_joined.fasta | fastx_barcode_splitter.pl --bcfile $TARGET --prefix loci/ --suffix ".fasta" --eol --mismatches $MISMATCHES --partial 0
+	mkdir $OUTPUT/$sample/loci
+	cat $OUTPUT/$sample/umi_joined.fasta | fastx_barcode_splitter.pl --bcfile $TARGET --prefix $OUTPUT/$sample/loci/ --suffix ".fasta" --eol --mismatches $MISMATCHES --partial 0
     
 	# trim target sequence (54 bp), remove UMI with N
-	mkdir umis
-	for i in loci/*.fasta; do
+	mkdir $OUTPUT/$sample/umis
+	for i in $OUTPUT/$sample/loci/*.fasta; do
 		locus=$(basename $i .fasta)
-		fastx_trimmer -t 54 -v -i $i | fastx_clipper -a X -l 6 -v > umis/$locus.umi.fasta
+		fastx_trimmer -t 54 -v -i $i | fastx_clipper -a X -l 6 -v > $OUTPUT/$sample/umis/$locus.umi.fasta
 	done
     
 	# merge identical UMIs
-	mkdir merged
-	for i in umis/*.umi.fasta; do
+	mkdir $OUTPUT/$sample/merged
+	for i in $OUTPUT/$sample/umis/*.umi.fasta; do
 		locus=$(basename $i .umi.fasta)
-		fastx_collapser -v -i $i > merged/$locus.merged.fasta
+		fastx_collapser -v -i $i > $OUTPUT/$sample/merged/$locus.merged.fasta
 	done
-	
-	cd ..
 done
