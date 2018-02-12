d4a4ff6a683371239ca0ec1dc9e34021fd6f39b9
Hindrek Teder <hindrek.teder@gmail.com>
2018-02-12 19:03:15 +0200

    add command-line arguments

--- sample.sh
+++ sample.sh
@@ -1,25 +1,40 @@
-# arguments with default values
-FASTQS=  # TODO: detect all FASTQs from bcl2fastq.sh output folder and subfolders
-TARGETS=  # target file format is based on FASTX Barcode Splitter barcode file format (http://hannonlab.cshl.edu/fastx_toolkit/commandline.html#fastx_barcode_splitter_usage)
-OUTPUT=
-MISMATCHES=5
+#!/bin/bash
+
+# read arguments
+INPUT1=$1  # compressed/uncompressed FASTQ files
+TARGET=$2  # target file format is based on FASTX Barcode Splitter barcode file format (http://hannonlab.cshl.edu/fastx_toolkit/commandline.html#fastx_barcode_splitter_usage)
+OUTPUT=$3
+MISMATCHES=$4  # number of allowed mismatches per target sequence
 
 mkdir $OUTPUT
 cd $OUTPUT
-for i in $FASTQS; do
-	sample=$(basename $i .fastq.gz)
-	mkdir $sample
-	cd $sample
+
+for i in $INPUT; do
+	# compressed FASTQ
+	if [[ $i == *.gz ]]; then
+		sample=$(basename $i .fastq.gz)  # remove .fastq.gz from sample name
+		mkdir $sample
+		cd $sample
+		
+		# convert FASTQ to FASTA, trim to 62 bp, keep 62 bp reads
+		zcat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > trimmed.fasta
 	
-	# FASTQ to FASTA, trim to 62 bp, keep 62 bp reads
-	zcat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > trimmed.fasta
-    
+	# uncompressed FASTQ
+	else
+		sample=$(basename $i .fastq)  # remove .fastq from sample name
+		mkdir $sample
+		cd $sample
+		
+		# convert FASTQ to FASTA, trim to 62 bp, keep 62 bp reads
+		cat $i | fastq_to_fasta -rnv -Q33 | fastx_trimmer -l 62 -v | fastx_clipper -a X -l 62 -n -v > trimmed.fasta
+	fi
+	
 	# join UMI subsequences
 	awk '{if (/^>/) print $0; else printf "%s%s%s\n", substr($0,1,4), substr($0,59,4), substr($0,5,54)}' trimmed.fasta > umi_joined.fasta
     
 	# demultiplex by locus
 	mkdir loci
-	cat umi_joined.fasta | fastx_barcode_splitter.pl --bcfile $TARGETS --prefix loci/ --suffix ".fasta" --eol --mismatches $MISMATCHES --partial 0
+	cat umi_joined.fasta | fastx_barcode_splitter.pl --bcfile $TARGET --prefix loci/ --suffix ".fasta" --eol --mismatches $MISMATCHES --partial 0
     
 	# trim target sequence (54 bp), remove UMI with N
 	mkdir umis
