444e5707c252db39b46eafd61095ec9a707be2b8
Hindrek Teder <hindrek.teder@gmail.com>
2018-03-05 22:38:57 +0200

    update variables

--- count.sh
+++ count.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # read arguments
-INPUT=$1  # prep.sh output
-UMI=$2  # UMI threshold
+input=$1  # prep.sh output
+umi=$2  # UMI threshold
 
 # write column headers to stdout
 echo -e "sample\tlocus\tread_count\tmolecule_count"
 
 # count by sample and locus
-for fasta in $INPUT/*/merged/*.merged.fasta
+for fasta in $input/*/merged/*.merged.fasta
 do
 	sample=$(basename $(dirname $(dirname $fasta)))
 	locus=$(basename $fasta .merged.fasta)
@@ -20,7 +20,7 @@ do
 	for count in $counts
 	do
 		read_count=$(($read_count + $count))  # total reads
-		if (($count >= $UMI))
+		if (($count >= $umi))
 		then
 			((molecule_count++))  # UMI corrected
 		fi
