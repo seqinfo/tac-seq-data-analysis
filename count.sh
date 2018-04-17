d70d70011e76f336aa4d315b72f17566bd1d2ac7
Hindrek Teder <hindrek.teder@gmail.com>
2018-04-17 22:39:50 +0300

    update style

--- count.sh
+++ count.sh
@@ -9,21 +9,20 @@ echo -e "sample\tlocus\tread_count\tmolecule_count"
 
 # count by sample and locus
 for fasta in $input/*/merged/*.merged.fasta; do
-	sample=$(basename $(dirname $(dirname $fasta)))
-	locus=$(basename $fasta .merged.fasta)
-	
-	# count reads and molecules
-	counts=$(grep '^>' $fasta | cut -d '-' -f 2)
-	read_count=0
-	molecule_count=0
-	for count in $counts; do
-		read_count=$(($read_count + $count))  # total reads
-		if (($count >= $umi))
-		then
-			((molecule_count++))  # UMI corrected
-		fi
-	done
-	
-	# write to stdout
-	echo -e "$sample\t$locus\t$read_count\t$molecule_count"
+  sample=$(basename $(dirname $(dirname $fasta)))
+  locus=$(basename $fasta .merged.fasta)
+  
+  # count reads and molecules
+  counts=$(grep '^>' $fasta | cut -d '-' -f 2)
+  read_count=0
+  molecule_count=0
+  for count in $counts; do
+    read_count=$(($read_count + $count))  # total reads
+    if (($count >= $umi)); then
+      ((molecule_count++))  # UMI corrected
+    fi
+  done
+  
+  # write to stdout
+  echo -e "$sample\t$locus\t$read_count\t$molecule_count"
 done
